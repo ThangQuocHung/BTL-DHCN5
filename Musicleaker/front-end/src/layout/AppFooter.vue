585bc19c785f42e1f1eabfa23b1e74f459356500
maianhnguyen670 <maianhnguyen670@gmail.com>
2021-10-25 17:53:17 +0700

    Update AppFooter.vue

--- Musicleaker/front-end/src/layout/AppFooter.vue
+++ Musicleaker/front-end/src/layout/AppFooter.vue
@@ -40,3 +40,14 @@
     </div>
   </footer>
 </template>
+<script>
+export default {
+  name: "app-footer",
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
