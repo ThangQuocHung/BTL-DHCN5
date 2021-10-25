2074137758d68b0ad3fdbf63894dc14d949e35c1
maianhnguyen670 <maianhnguyen670@gmail.com>
2021-10-25 17:36:45 +0700

    Create AppFooter.vue

---
+++ Musicleaker/front-end/src/layout/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer has-cards">
+    <div class="container">
+      <hr />
+      <div class="row align-items-center justify-content-md-between">
+        <div class="col-md-6">
+          <div class="copyright">
+            &copy; {{ year }}
+            <a
+              href="http://localhost:8080/"
+              target="_blank"
+              rel="noopener"
+              >MusicLeaker v2.0</a
+            >
+          </div>
+        </div>
+  </footer>
+</template>
