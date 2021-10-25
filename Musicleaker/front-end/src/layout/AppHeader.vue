d5a2e789e94e70d13d2fc3b0f40ca3ad894fa4f0
maianhnguyen670 <maianhnguyen670@gmail.com>
2021-10-25 16:10:32 +0700

    Create AppHeader.vue

---
+++ Musicleaker/front-end/src/layout/AppHeader.vue
@@ -0,0 +1,72 @@
+<template>
+  <header class="header-global">
+    <base-nav class="navbar-main" transparent type="" effect="light" expand>
+      <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
+        <img src="img/brand/white.png" alt="logo" />
+      </router-link>
+
+      <div class="row" slot="content-header" slot-scope="{ closeMenu }">
+        <div class="col-6 collapse-brand">
+          <a href=""><img src="img/brand/white.png" /></a>
+        </div>
+        <div class="col-6 collapse-close">
+          <close-button @click="closeMenu"></close-button>
+        </div>
+      </div>
+
+      <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
+        <div
+          slot="title"
+          href="#"
+          class="nav-link"
+          data-toggle="dropdown"
+          role="button"
+        >
+          <span class="nav-link-inner--text">Tìm và lưu trữ nhạc của bạn</span>
+        </div>
+      </ul>
+      <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="http://localhost:8080/"
+            target="_blank"
+            rel="noopener"
+            data-toggle="tooltip"
+            title="Like us on Facebook"
+          >
+            <i class="fa fa-facebook-square"></i>
+            <span class="nav-link-inner--text d-lg-none">Facebook</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="http://localhost:8080/"
+            target="_blank"
+            rel="noopener"
+            data-toggle="tooltip"
+            title="Star us on Github"
+          >
+            <i class="fa fa-github"></i>
+            <span class="nav-link-inner--text d-lg-none">Github</span>
+          </a>
+        </li>
+      </ul>
+    </base-nav>
+  </header>
+</template>
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    BaseDropdown
+  }
+};
+</script>
+<style></style>
