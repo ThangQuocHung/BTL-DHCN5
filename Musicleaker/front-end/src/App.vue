2cad22f733ee691194fee9cbaeb13ca3f4fba021
ThangQuocHung <90396772+ThangQuocHung@users.noreply.github.com>
2021-10-25 16:50:42 +0700

    update BTL

--- Musicleaker/front-end/src/App.vue
+++ Musicleaker/front-end/src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view name="header"></router-view>
+        <main>
+            <fade-transition origin="center" mode="out-in" :duration="250">
+                <router-view/>
+            </fade-transition>
+        </main>
+        <router-view name="footer"></router-view>
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'App'
-}
-</script>
+import { FadeTransition } from "vue2-transitions";
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  components: {
+    FadeTransition
+  }
+};
+</script>
--- Musicleaker/front-end/src/main.js
+++ Musicleaker/front-end/src/main.js
@@ -1,15 +1,38 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+/*!
 
-Vue.config.productionTip = false
+=========================================================
+* Vue Argon Design System - v1.1.0
+=========================================================
 
-/* eslint-disable no-new */
+* Product Page: https://www.creative-tim.com/product/argon-design-system
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md)
+
+* Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Argon from "./plugins/argon-kit";
+import './registerServiceWorker'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+import VueSession from 'vue-session'
+
+Vue.config.productionTip = false;
+Vue.use(Argon);
+Vue.use(VueAxios, axios);
+Vue.use(ViewUI);
+Vue.use(VueSession);
+
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount("#app");
