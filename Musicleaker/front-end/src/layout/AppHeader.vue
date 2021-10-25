b1f401bc4d22c0f0bba1364fccfb56e64b460f79
maianhnguyen670 <maianhnguyen670@gmail.com>
2021-10-25 17:29:46 +0700

    Update AppHeader.vue

--- Musicleaker/front-end/src/layout/AppHeader.vue
+++ Musicleaker/front-end/src/layout/AppHeader.vue
@@ -55,3 +55,17 @@
     </base-nav>
   </header>
 </template>
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    BaseDropdown
+  }
+};
+</script>
+<style></style>
