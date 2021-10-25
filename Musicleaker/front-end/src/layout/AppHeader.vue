8be1621363dee0d4a7225f83b2906e8c38d0d619
maianhnguyen670 <maianhnguyen670@gmail.com>
2021-10-25 16:57:29 +0700

    Create AppHeader.vue

---
+++ Musicleaker/front-end/src/layout/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header-global">
+    <base-nav class="navbar-main" transparent type="" effect="light" expand>
+      <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
+        <img src="img/brand/white.png" alt="logo" />
+      </router-link>
+
+      <div class="row" slot="content-header" slot-scope="{ closeMenu }">
+        <div class="col-6 collapse-brand">
+          <a href=""><img src="img/brand/white.png" /></a>
+        </div>
+        <div class="col-6 collapse-close">
+          <close-button @click="closeMenu"></close-button>
+        </div>
+      </div>
+</template>
